24168b2f2a9185ce49668a518b7afa260a9836a5
Cristiano Batista <crisaodev@outlook.com>
2026-08-15 17:40:38 -0300

    feat: create logs for thrown exceptions and for stats and transaction services

--- src/main/java/com/itau/challenge/infra/RestExceptionHandler.java
+++ src/main/java/com/itau/challenge/infra/RestExceptionHandler.java
@@ -2,12 +2,14 @@ package com.itau.challenge.infra;
 
 import com.itau.challenge.infra.exceptions.BadTransactionException;
 import com.itau.challenge.infra.exceptions.UnprocessableEntityException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
@@ -22,6 +24,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     private ResponseEntity<RestBodyResponse> handleException(HttpStatus status, String message){
+        log.error(message);
         RestBodyResponse body = new RestBodyResponse(status, message);
         return ResponseEntity.status(body.getStatus()).body(body);
     }
--- src/main/java/com/itau/challenge/services/StatsService.java
+++ src/main/java/com/itau/challenge/services/StatsService.java
@@ -3,18 +3,21 @@ package com.itau.challenge.services;
 import com.itau.challenge.dtos.StatsDTO;
 import com.itau.challenge.models.Transaction;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
-@Service
+@Slf4j
 @RequiredArgsConstructor
+@Service
 public class StatsService {
     private final TransactionService transactionService;
 
     public StatsDTO getStats(int seconds){
+        log.info("Getting stats for {} seconds", seconds);
         OffsetDateTime end = OffsetDateTime.now();
         OffsetDateTime start = end.minusSeconds(seconds);
 
@@ -22,7 +25,8 @@ public class StatsService {
         DoubleSummaryStatistics stats = transactions.stream()
                 .mapToDouble(Transaction::getValue)
                 .summaryStatistics();
-
-        return new StatsDTO(stats);
+        StatsDTO statsDTO = new StatsDTO(stats);
+        log.info("Stats for {} seconds is {}", seconds, stats);
+        return statsDTO;
     }
 }
