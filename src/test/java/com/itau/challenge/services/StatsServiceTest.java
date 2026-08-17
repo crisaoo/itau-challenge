62735ffa8f6f07f8eeafa4399944f2768a55ebab
Cristiano Batista <crisaodev@outlook.com>
2026-08-17 16:07:42 -0300

    feat: update tests

--- src/test/java/com/itau/challenge/services/StatsServiceTest.java
+++ src/test/java/com/itau/challenge/services/StatsServiceTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class StatsServiceTest {
@@ -28,36 +28,36 @@ public class StatsServiceTest {
     @Test
     @DisplayName("Should calculate the stats correctly")
     void getStatsCase1() {
+        // arrange
         OffsetDateTime now = OffsetDateTime.now();
-
         List<Transaction> transactions = List.of(
                 new Transaction(UUID.randomUUID(), 100.00, now.minusSeconds(10)),
                 new Transaction(UUID.randomUUID(), 200.00, now.minusSeconds(20)),
                 new Transaction(UUID.randomUUID(), 300.00, now.minusSeconds(30))
         );
-
+        StatsDTO expectedResult = new StatsDTO(3, 600.00, 200.00, 100.00, 300.00);
         when(transactionService.getTransactionsByDateTimeBetween(any(), any())).thenReturn(transactions);
 
+        // act
         StatsDTO result = service.getStats(60);
 
-        Assertions.assertEquals(3, result.count());
-        Assertions.assertEquals(600.00, result.sum());
-        Assertions.assertEquals(200.00, result.avg());
-        Assertions.assertEquals(100.00, result.min());
-        Assertions.assertEquals(300.00, result.max());
+        // assert
+        verify(transactionService, times(1)).getTransactionsByDateTimeBetween(any(), any());
+        Assertions.assertEquals(expectedResult, result);
     }
 
     @Test
     @DisplayName("Should return 0 for all stats fields when there aren't transactions")
     void getStatsCase2() {
+        // arrange
+        StatsDTO expectedResult = new StatsDTO(0, 0.0, 0.0, 0.0, 0.0);
         when(transactionService.getTransactionsByDateTimeBetween(any(), any())).thenReturn(List.of());
 
+        // act
         StatsDTO result = service.getStats(30);
 
-        Assertions.assertEquals(0, result.count());
-        Assertions.assertEquals(0.00, result.sum());
-        Assertions.assertEquals(0.00, result.avg());
-        Assertions.assertEquals(0.00, result.min());
-        Assertions.assertEquals(0.00, result.max());
+        // assert
+        verify(transactionService, times(1)).getTransactionsByDateTimeBetween(any(), any());
+        Assertions.assertEquals(expectedResult, result);
     }
 }
--- src/test/java/com/itau/challenge/services/TransactionServiceTest.java
+++ src/test/java/com/itau/challenge/services/TransactionServiceTest.java
@@ -15,6 +15,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.UUID;
 
 import static org.mockito.Mockito.*;
 
@@ -30,52 +32,94 @@ class TransactionServiceTest {
     @Test
     @DisplayName("Should create a transaction successfully")
     void createTransactionCase1() {
+        // arrange
         TransactionDTO dto = new TransactionDTO(3000.0, OffsetDateTime.now());
-        service.createTransaction(dto);
+        Transaction transactionSaved = new Transaction(UUID.randomUUID(), dto.value(), dto.dateTime());
+        when(repository.save(any(Transaction.class))).thenReturn(transactionSaved);
 
+        // act
+        Transaction result = service.createTransaction(dto);
+
+        // asserts
+        Assertions.assertEquals(transactionSaved, result);
         verify(repository, times(1)).save(any(Transaction.class));
     }
 
     @Test
     @DisplayName("Should throw a BadTransactionException when any field is empty")
     void createTransactionCase2() throws BadTransactionException {
-        BadTransactionException thrown = Assertions.assertThrows(BadTransactionException.class, () -> {
-            TransactionDTO dto = new TransactionDTO(null, null);
-            service.createTransaction(dto);
-        });
+        // arrange
+        TransactionDTO dto = new TransactionDTO(null, null);
 
+        // act
+        BadTransactionException thrown = Assertions.assertThrows(BadTransactionException.class,
+                () -> service.createTransaction(dto)
+        );
+
+        // assert
         Assertions.assertEquals("Transaction date and value cannot be empty", thrown.getMessage());
-        verify(repository, times(0)).save(any(Transaction.class));
+        verify(repository, never()).save(any());
     }
 
     @Test
     @DisplayName("Should throw a IllegalArgumentException when the date is in the future")
     void createTransactionCase3() {
-        UnprocessableEntityException thrown = Assertions.assertThrows(UnprocessableEntityException.class, () -> {
-           TransactionDTO dto = new TransactionDTO(3000.0, OffsetDateTime.now().plusSeconds(1));
-           service.createTransaction(dto);
-        });
+        // arrange
+        TransactionDTO dto = new TransactionDTO(3000.0, OffsetDateTime.now().plusSeconds(1));
 
+        // act
+        UnprocessableEntityException thrown = Assertions.assertThrows(UnprocessableEntityException.class,
+                () -> service.createTransaction(dto)
+        );
+
+        // assert
         Assertions.assertEquals("Transaction date cannot be in the future", thrown.getMessage());
-        verify(repository, times(0)).save(any(Transaction.class));
+        verify(repository, never()).save(any(Transaction.class));
     }
 
     @Test
     @DisplayName("Should throw a IllegalArgumentException when the value is negative")
     void createTransactionCase4() {
-        UnprocessableEntityException thrown = Assertions.assertThrows(UnprocessableEntityException.class, () -> {
-            TransactionDTO dto = new TransactionDTO(-500.0, OffsetDateTime.now());
-            service.createTransaction(dto);
-        });
+        // arrange
+        TransactionDTO dto = new TransactionDTO(-500.0, OffsetDateTime.now());
 
+        // act
+        UnprocessableEntityException thrown = Assertions.assertThrows(UnprocessableEntityException.class,
+                () -> service.createTransaction(dto)
+        );
+
+        // assert
         Assertions.assertEquals("Transaction value cannot be negative", thrown.getMessage());
-        verify(repository, times(0)).save(any(Transaction.class));
+        verify(repository, never()).save(any(Transaction.class));
     }
 
     @Test
     @DisplayName("Should delete all transactions in database")
     void deleteAllTransactions() {
+        // act
         service.deleteAllTransactions();
+
+        // assert
         verify(repository, times(1)).deleteAll();
+    }
+
+    @Test
+    @DisplayName("Should get a list of transactions in database")
+    void getTransactionsByDateTimeBetween(){
+        // arrange
+        OffsetDateTime now = OffsetDateTime.now();
+        List<Transaction> expectedResult = List.of(
+                new Transaction(UUID.randomUUID(), 100.00, now.minusSeconds(10)),
+                new Transaction(UUID.randomUUID(), 200.00, now.minusSeconds(20)),
+                new Transaction(UUID.randomUUID(), 300.00, now.minusSeconds(30))
+        );
+        when(repository.findByDateTimeBetween(any(OffsetDateTime.class), any(OffsetDateTime.class))).thenReturn(expectedResult);
+
+        // act
+        List<Transaction> result = service.getTransactionsByDateTimeBetween(now.minusSeconds(60), now);
+
+        // assert
+        verify(repository, times(1)).findByDateTimeBetween(any(OffsetDateTime.class), any(OffsetDateTime.class));
+        Assertions.assertEquals(expectedResult, result);
     }
 }
